52b119ad9e5bffefcbb787b3e1446c6ad7fd0f39
Taylor Huston <taylor.a.huston@gmail.com>
2016-01-29 23:44:51 -0700

    Add Watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,70 +9,86 @@ module.exports = function (grunt) {
   // ===========================================================================
   grunt.initConfig({
 
-    // get the configuration info from package.json ----------------------------
-    // this way we can use things like name and version (pkg.name)
-    pkg: grunt.file.readJSON('package.json'),
+      // get the configuration info from package.json ----------------------------
+      // this way we can use things like name and version (pkg.name)
+      pkg: grunt.file.readJSON('package.json'),
 
-    // all of our configuration will go here
+      // all of our configuration will go here
 
-    // configure jshint to validate js files -----------------------------------
-    jshint: {
-      options: {
-        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      // configure jshint to validate js files -----------------------------------
+      jshint: {
+        options: {
+          reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+        },
+
+        // when this task is run, lint the Gruntfile and all js files in src
+        build: ['Gruntfile.js', 'src/**/*.js']
+
       },
 
-      // when this task is run, lint the Gruntfile and all js files in src
-      build: ['Gruntfile.js', 'src/**/*.js']
-  
-    },
-
-    // configure uglify to minify js files -------------------------------------
-    uglify: {
-      options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      // configure uglify to minify js files -------------------------------------
+      uglify: {
+        options: {
+          banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+        },
+        build: {
+          files: {
+            'dist/js/magic.min.js': ['src/js/magic.js', 'src/js/magic2.js']
+          }
+        }
       },
-      build: {
-        files: {
-          'dist/js/magic.min.js': ['src/js/magic.js', 'src/js/magic2.js']
-        }
-      }
-    },
 
-    less: {
-      build: {
-        files: {
-          'dist/css/pretty.css': 'src/css/pretty.less'
+      less: {
+        build: {
+          files: {
+            'dist/css/pretty.css': 'src/css/pretty.less'
+          }
         }
-      }
-    },
-
-    // configure cssmin to minify css files ------------------------------------
-    cssmin: {
-      options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
       },
-      build: {
-        files: {
-          'dist/css/style.min.css': 'src/css/style.css'
+
+      // configure cssmin to minify css files ------------------------------------
+      cssmin: {
+        options: {
+          banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+        },
+        build: {
+          files: {
+            'dist/css/style.min.css': 'src/css/style.css'
+          }
         }
+      },
+
+      // configure watch to auto update ----------------
+      watch: {
+
+        // for stylesheets, watch css and less files 
+        // only run less and cssmin stylesheets: { 
+        files: ['src//*.css', 'src//*.less'],
+        tasks: ['less', 'cssmin']
+      },
+
+      // for scripts, run jshint and uglify 
+      scripts: {
+        files: 'src/**/*.js',
+        tasks: ['jshint', 'uglify']
       }
     }
 
 
   });
 
-  // ===========================================================================
-  // LOAD GRUNT PLUGINS ========================================================
-  // ===========================================================================
-  // we can only load these if they are in our package.json
-  // make sure you have run npm install so our app can find these
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+// ===========================================================================
+// LOAD GRUNT PLUGINS ========================================================
+// ===========================================================================
+// we can only load these if they are in our package.json
+// make sure you have run npm install so our app can find these
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // ============= // CREATE TASKS ========== //
-  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'less']);
+// ============= // CREATE TASKS ========== //
+grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'less']);
 
 };
